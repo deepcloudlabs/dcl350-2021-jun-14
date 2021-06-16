a790e1e8a5001990d6a32460e1e356f1f632c9e5
deepcloudlabs <deepcloudlabs@gmail.com>
2021-06-16 18:44:14 +0300

    add i18n for bean validation

--- hr-microservice/src/main/java/com/example/hr/controller/HrController.java
+++ hr-microservice/src/main/java/com/example/hr/controller/HrController.java
@@ -2,6 +2,7 @@ package com.example.hr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +16,13 @@ import com.example.hr.boundary.FireEmployeeResponse;
 import com.example.hr.boundary.HireEmployeeRequest;
 import com.example.hr.boundary.HireEmployeeResponse;
 import com.example.hr.service.HrService;
+import com.example.validation.TcKimlik;
 
 @RestController
 @RequestScope
 @RequestMapping("employees")
 @CrossOrigin
+@Validated
 public class HrController { // Adapter -> Protocol [HTTP] Adapter
 	@Autowired 
 	private HrService hrService;
@@ -30,7 +33,7 @@ public class HrController { // Adapter -> Protocol [HTTP] Adapter
 	}
 
 	@DeleteMapping("{kimlik}")
-	public FireEmployeeResponse fireEmployee(@PathVariable String kimlik){
+	public FireEmployeeResponse fireEmployee(@PathVariable @TcKimlik String kimlik){
 		return hrService.fireEmployee(kimlik);		
 	}
 }
