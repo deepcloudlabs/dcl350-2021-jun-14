67e2b05f4186d82ac5ed097653e9525400f5b2b7
deepcloudlabs <deepcloudlabs@gmail.com>
2021-06-17 10:45:10 +0300

    initial commit

--- lottery-microservice/src/main/java/com/example/lottery/controller/LotteryController.java
+++ lottery-microservice/src/main/java/com/example/lottery/controller/LotteryController.java
@@ -3,26 +3,31 @@ package com.example.lottery.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
 import com.example.lottery.application.LotteryApplication;
 
 @RestController
-@RequestScope
+@RefreshScope
 @RequestMapping("/numbers")
 @CrossOrigin
 public class LotteryController {
 	@Autowired
 	private LotteryApplication lotteryApplication;
+	@Value("${lotteryMax}")
+	private int lotteryMax;
+	@Value("${lotterySize}")
+	private int lotterySize;
 	
 	// http://localhost:5100/lottery/api/v1/numbers?column=10
 	@GetMapping
 	public List<List<Integer>> draw(@RequestParam int column){
-		return lotteryApplication.draw(60, 6, column);
+		return lotteryApplication.draw(lotteryMax, lotterySize, column);
 	}
 }
