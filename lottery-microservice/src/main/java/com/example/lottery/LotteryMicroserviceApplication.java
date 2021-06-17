2d8ff810fc1c7c52a850845708d9d8a37c5615d2
deepcloudlabs <deepcloudlabs@gmail.com>
2021-06-17 12:34:55 +0300

    add explanations about running and configuraing spring boot applications

--- lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
+++ lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
@@ -4,6 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+// How to build the jar using maven:
+// mvn clean install spring-boot:repackage
+
+// How to run the spring boot application using maven:
+// mvn clean install spring-boot:run
+
+// How to over-write the parameters in application.properties:
+//  i) java -Dserver.port=4200 -DlotteryMax=100 -DlotterySize=10 -jar lottery.jar
+
+// ii) jar xvf lottery.jar BOOT-INF/classes/application.properties
+//     move BOOT-INF\classes\application.properties .
+//     java -cp . -jar lottery.jar  ==> now spring uses the extracted appication.properties file  
 
 @SpringBootApplication
 @EnableDiscoveryClient
